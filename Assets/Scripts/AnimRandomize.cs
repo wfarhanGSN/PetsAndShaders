7d608351fa92dd68d180b9e23cbac7f0009b8bf8
wajdyFarhan <wfarhan@gsngames.com>
2019-05-02 14:23:28 +0100

    AnimRandomize and dog controller

---
+++ Assets/Scripts/AnimRandomize.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class AnimRandomize : StateMachineBehaviour
+{
+    [SerializeField]
+    private int[] weights;
+    private int total;
+
+    void Awake()
+    {
+        total=0;
+        for(var i = 0; i< weights.Length; i++){
+            total += weights[i];
+        }
+
+    }
+    
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        int pick = Random.Range(1,total+1); //range is max exclusive
+
+        int cumTotal=0;
+        for(var i = 0; i< weights.Length; i++)
+        {
+            cumTotal+=weights[i];
+            if (pick<=cumTotal)
+            {
+                animator.SetInteger("AnimRandomize",i);
+                break;
+            }
+        }
+    }
+}
