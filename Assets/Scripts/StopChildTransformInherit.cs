e2812cad1bdfddae075f5abfc093b17d7496e378
wajdyFarhan <wfarhan@gsngames.com>
2019-05-24 15:28:38 +0100

    anim fixes

---
+++ Assets/Scripts/StopChildTransformInherit.cs
@@ -0,0 +1,24 @@
+﻿    using System.Collections.Generic;
+    using System.Collections;
+    using UnityEngine;
+
+    public class StopChildTransformInherit : MonoBehaviour
+    {
+
+        Vector3 initialPosition;
+        Quaternion initialRotation;
+        Vector3 initialScale;
+        void Start ()
+        {
+            initialPosition = transform.position;
+            initialRotation = transform.rotation;
+            initialScale = transform.localScale;
+        }
+
+        void Update ()
+        {
+            transform.rotation = initialRotation;
+            transform.position = initialPosition;
+            transform.localScale = initialScale;
+        }
+    }
